248c27a2e2ceefbc18ce6c2564731ae24f26d556
Kartik <100852087+cipher997@users.noreply.github.com>
2023-03-28 04:52:59 +0530

    cta-footer-changesfixed

--- src/components/CTA/Cta.jsx
+++ src/components/CTA/Cta.jsx
@@ -3,20 +3,45 @@ import "./Cta.scss";
 
 const Cta = () => {
   return (
-    <div className="cta-content strong white">
-      <h1>Pune, MH Pest Exterminator</h1>
-      <h2>Pune Number 1 Pest Exterminator is Just 1 Call Away!</h2>
-      <p>
-        For Fast & Easy, Same-Day Service CALL NOW to Get Rid of Those Pesky
-        Pests Today!
-      </p>
-      <p className="cta-btn">(877) 846-0510</p>
-      <div className="cta-list">
-        <ul>
-          <li>No-Obligation Estimates</li>
-          <li>24-Hours / 7 Days a Week</li>
-          <li>100% Quality Guaranteed</li>
-        </ul>
+    <div className="cta-section text-align-center">
+      <div className="cta-content white">
+        <div className="row">
+          <div className="cta-1">
+            <h1>Pune, MH Pest Exterminator</h1>
+            <h2>Pune Number 1 Pest Exterminator is Just 1 Call Away!</h2>
+            <p>
+              For Fast & Easy, Same-Day Service <br />
+              CALL NOW to Get Rid of Those Pesky Pests Today!
+            </p>
+            <p className="cta-btn bg-red">(877) 846-0510</p>
+          </div>
+          <ul className="first">
+            <li>
+              <div className="feature">
+                <div className="feature-icon">
+                  <i className="icon icon-ok-circled">&#xe802;</i>
+                </div>
+                <div className="feature-tag">No-Obligation Estimates</div>
+              </div>
+            </li>
+            <li>
+              <div className="feature">
+                <div className="feature-icon">
+                  <i className="icon icon-ok-circled">&#xe802;</i>
+                </div>
+                <div className="feature-tag">24-Hours / 7 Days a Week</div>
+              </div>
+            </li>
+            <li>
+              <div className="feature">
+                <div className="feature-icon">
+                  <i className="icon icon-ok-circled">&#xe802;</i>
+                </div>
+                <div className="feature-tag">100% Quality Guaranteed</div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,27 +3,34 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div className="footer-content bg-green grey">
-      <div className="footer-1">
-        <h6>EXTERMINATORS PRO</h6>
-        <p>© 2022 exterminatorspros.com — All Rights Reserved</p>
+    <footer className="bg-green grey">
+      <div class="container">
+        <div class="row">
+          <div class="logo-holder">
+            <h2>Exterminators Pro</h2>
+            <p class="copyright">
+              © 2022 exterminatorspros.com — All Rights Reserved
+            </p>
+          </div>
+          <div class="discalimer">
+            <p>
+              Exterminators Pros is a free referral service that helps users
+              connect with local service providers. Our service includes a
+              nationwide network of contractors and a 24/7 customer support and
+              dispatch center. All contractors are independent and Exterminators
+              Pros does not endorse or guarantee any work performed. It is the
+              responsibility of the user to verify the contractor they hire has
+              the necessary license and insurance required for the work being
+              performed. Any third party products, brands or trademarks listed
+              above are the sole property of their respective owner. No
+              affiliation or endorsement is intended or implied. All persons
+              depicted are actors or models and not contractors listed on
+              Exterminators Pros.
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="footer-2">
-        <p>
-          Exterminators Pros is a free referral service that helps users connect
-          with local service providers. Our service includes a nationwide
-          network of contractors and a 24/7 customer support and dispatch
-          center. All contractors are independent and Exterminators Pros does
-          not endorse or guarantee any work performed. It is the responsibility
-          of the user to verify the contractor they hire has the necessary
-          license and insurance required for the work being performed. Any third
-          party products, brands or trademarks listed above are the sole
-          property of their respective owner. No affiliation or endorsement is
-          intended or implied. All persons depicted are actors or models and not
-          contractors listed on Exterminators Pros.
-        </p>
-      </div>
-    </div>
+    </footer>
   );
 };
 
